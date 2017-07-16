844a8e07bc0ed8fa915e2c46104bbefd9a3a3214
Shane Lillie <ignignokterr@gmail.com>
2017-07-15 20:50:04 -0700

    More work ont he Unity controller

--- Assets/Scripts/AirConsoleController.cs
+++ Assets/Scripts/AirConsoleController.cs
@@ -1,4 +1,5 @@
 using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 using UnityEngine;
 
@@ -9,5 +10,21 @@ namespace CatFight
     [RequireComponent(typeof(AirConsole))]
     public class AirConsoleController : MonoBehavior
     {
+        private void Start()
+        {
+            AirConsole.instance.onMessage += OnMessage;
+        }
+
+        private void OnDestroy()
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+
+        private void OnMessage(int from, JToken data)
+        {
+            Debug.Log($"OnMessage({from}: {data})");
+
+            AirConsole.instance.Message(from, "Hello World!");
+        }
     }
 }
