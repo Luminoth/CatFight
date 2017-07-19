1b5069746738170c6779e57b07defb251850d3d8
Shane Lillie <ignignokterr@gmail.com>
2017-07-18 22:03:51 -0700

    Added countdown/timer to arena and loop back to the lobby on finish

--- Assets/Scripts/Scenes/Arena.cs
+++ Assets/Scripts/Scenes/Arena.cs
@@ -1,8 +1,75 @@
-﻿using CatFight.Util;
+﻿using System;
+using System.Collections;
+
+using CatFight.Util;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace CatFight.Scenes
 {
     public sealed class Arena : SingletonBehavior<Arena>
     {
+        [SerializeField]
+        private int _countdownSeconds = 5;
+
+        [SerializeField]
+        private Text _countdownText;
+
+        [SerializeField]
+        private int _fightTimeSeconds = 120;
+
+        [SerializeField]
+        private Text _timerText;
+
+        [SerializeField]
+        [ReadOnly]
+        private int _actualFightTimeSeconds;
+
+        private void Start()
+        {
+            _countdownText.text = _countdownSeconds.ToString();
+            _timerText.text = _fightTimeSeconds.ToString();
+
+            StartCoroutine(Countdown(() => {
+                StartCoroutine(Timer(() => {
+                    GameStageManager.Instance.LoadLobby();
+                }));
+            }));
+        }
+
+        private IEnumerator Countdown(Action callback)
+        {
+            DateTime start = DateTime.Now;
+            DateTime end = start.AddSeconds(_countdownSeconds);
+
+            TimeSpan remaining = end - DateTime.Now;
+            while(remaining.TotalSeconds > 0) {
+                _countdownText.text = ((int)remaining.TotalSeconds).ToString();
+                yield return new WaitForSeconds(1);
+                remaining = end - DateTime.Now;
+            }
+
+            callback?.Invoke();
+        }
+
+        private IEnumerator Timer(Action callback)
+        {
+            DateTime start = DateTime.Now;
+
+            _actualFightTimeSeconds = _fightTimeSeconds;
+            while(true) {
+                DateTime end = start.AddSeconds(_actualFightTimeSeconds);
+                TimeSpan remaining = end - DateTime.Now;
+                if(remaining.TotalSeconds <= 0) {
+                    break;
+                }
+
+                _timerText.text = ((int)remaining.TotalSeconds).ToString();
+                yield return new WaitForSeconds(1);
+            }
+
+            callback?.Invoke();
+        }
     }
 }
--- TODO.md
+++ TODO.md
@@ -1,7 +1,12 @@
 SHANE
 ==========
 
-Need to hack the controller to start setting/clearing slots
+Loading manager (to better control the loading order, should display load step on teh loading screen)
+    This should also create the required managers (which may need to come from prefabs if they have configurable state)
+    This should destroy itself when it's done loading stuff
+Game data manager (move this code out of the AirConsoleController)
+Rename the AirConsoleController to AirConsoleManager
+Player teams!
 
 TIEN
 ==========
