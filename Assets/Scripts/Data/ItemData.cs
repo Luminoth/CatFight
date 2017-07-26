1744f747e697997a70b85d6f46bfdb64810d954f
Shane Lillie <ignignokterr@gmail.com>
2017-07-25 22:59:28 -0700

    Idk

---
+++ Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace CatFight.Data
+{
+    [Serializable]
+    public abstract class ItemData
+    {
+        [SerializeField]
+        private int _id;
+
+        public int Id => _id;
+
+        [SerializeField]
+        private string _name;
+
+        public string Name => _name;
+    }
+}
