609d212686b97626f1c2e33171a33323ced30f32
Shane Lillie <ignignokterr@gmail.com>
2017-08-11 09:03:32 -0700

    Chaff skeleton

---
+++ Assets/Scripts/Items/Specials/Chaff.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using CatFight.Util.ObjectPool;
+
+using UnityEngine;
+
+namespace CatFight.Items.Specials
+{
+    [RequireComponent(typeof(PooledObject))]
+    public sealed class Chaff : SpecialAmmo
+    {
+        private PooledObject _pooledObject;
+
+#region Unity Lifecycle
+        private void Awake()
+        {
+            _pooledObject = GetComponent<PooledObject>();
+            _pooledObject.RecycleEvent += RecycleEventHandler;
+        }
+#endregion
+
+        protected override void OnFighterCollision()
+        {
+            base.OnFighterCollision();
+
+            _pooledObject.Recycle();
+        }
+
+        protected override void OnArenaCollision()
+        {
+            base.OnArenaCollision();
+
+            _pooledObject.Recycle();
+        }
+
+#region Event Handlers
+        private void RecycleEventHandler(object sender, EventArgs evt)
+        {
+            Destroy();
+        }
+#endregion
+    }
+}
